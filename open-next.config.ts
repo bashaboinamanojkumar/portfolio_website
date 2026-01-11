deb012879dc8977dc32bfcf24c2c326d4c50d6f3
bashaboinamanojkumar <manoj7@umd.edu>
2026-01-11 01:49:11 -0500

    Add missing OpenNext config to fix CI freeze

---
+++ open-next.config.ts
@@ -0,0 +1,5 @@
+import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+
+export default defineCloudflareConfig({
+    // This enables the default configuration for Cloudflare Workers
+});
